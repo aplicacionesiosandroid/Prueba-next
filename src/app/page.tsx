7171d59c95310ab8b24845550f701cacc339c1ea
Subhasish Negel <negelsubho@gmail.com>
2024-01-15 20:30:22 +0530

    made some changes on homepage for better understanding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
+
 const page = () => {
   return (
-    <div className="flex text-4xl justify-center ">
-      <h1 className="">This is HOME PAGE</h1>
-      <h2>
-        Please Visit Other Pages from the Navbar to See & Test Functionalities
+    <div className="flex flex-col items-center text-wrap">
+      <h1 className="text-4xl">This is HOME PAGE</h1>
+      <h2 className="text-3xl">
+        Please Visit Other Pages from Navbar to See & Test Functionalities
       </h2>
+      <p className="text-2xl">
+        Checkout the source codes{" "}
+        <Link
+          target="_blank"
+          className="text-blue-500 hover:text-blue-600 hover:underline"
+          href="https://github.com/Subhasish-Negel/zustand-product-store"
+        >
+          here
+        </Link>
+      </p>
     </div>
   );
 };
