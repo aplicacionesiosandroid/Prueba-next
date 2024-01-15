6ab50ab8105a844bd65d28ca72a6d5e511b916a9
Subhasish Negel <negelsubho@gmail.com>
2024-01-15 18:08:21 +0530

    home page update xD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 const page = () => {
   return (
     <div className="flex text-4xl justify-center ">
-      
       <h1 className="">This is HOME PAGE</h1>
+      <h2>
+        Please Visit Other Pages from the Navbar to See & Test Functionalities
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
